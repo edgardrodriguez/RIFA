6d3ef66d7669898c8b58118ff062b293d327225f
Fernando Canales <Fernando Canales@FernandoCanales>
2021-05-20 20:44:43 -0500

    Se implementó el CRUD de la tabla PREMIO. La implementación del CRUD en la tabla Ticket está en progreso.

--- RIFA/src/main/java/bean/PremioC.java
+++ RIFA/src/main/java/bean/PremioC.java
@@ -5,10 +5,94 @@
  */
 package bean;
 
+import modelo.PremioModelo;
+import dao.PremioDao;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.util.List;
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.annotation.PostConstruct;
+
 /**
  *
  * @author Pablo Canales
  */
+@Named(value="premioC")
+@SessionScoped
 public class PremioC {
+    private PremioDao dao;
+    private PremioModelo modelo;
+    private List<PremioModelo> listaPremio;
     
+    public PremioC(){
+        modelo = new PremioModelo();
+        dao = new PremioDao();
+        listaPremio = new ArrayList();
+    }
+    
+    public void listar(){
+        try {
+            listaPremio = dao.listar();
+        } catch (Exception e) {
+            System.out.println("Error en PremioControlador-Listar: "+ e.getMessage());
+        }
+    }
+    
+    @PostConstruct
+    public void construir(){
+        listar();
+    }
+    
+    public void registrar(){
+        try {
+            dao.registrar(modelo);
+            listar();
+            limpiar();
+        } catch (Exception e) {
+            System.out.println("Error En Premiocontrolador-registrar: "+ e.getMessage());
+        }
+    }
+    
+    public void eliminar(PremioModelo pre){
+        try {
+            dao.eliminar(pre);
+            listar();
+            limpiar();
+        } catch (Exception e) {
+            System.out.println("Error en PersonaControlador-Eliminar: "+ e.getMessage());
+        }
+    }
+    
+    public void datos(PremioModelo pre){
+        this.modelo = pre;
+    }
+    
+    public void limpiar(){
+        modelo = new PremioModelo();
+    }
+
+    public PremioDao getDao() {
+        return dao;
+    }
+
+    public void setDao(PremioDao dao) {
+        this.dao = dao;
+    }
+
+    public PremioModelo getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(PremioModelo modelo) {
+        this.modelo = modelo;
+    }
+
+    public List<PremioModelo> getListaPremio() {
+        return listaPremio;
+    }
+
+    public void setListaPremio(List<PremioModelo> listaPremio) {
+        this.listaPremio = listaPremio;
+    }
 }
--- RIFA/src/main/java/dao/PremioDao.java
+++ RIFA/src/main/java/dao/PremioDao.java
@@ -5,10 +5,90 @@
  */
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import modelo.PremioModelo;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Pablo Canales
  */
-public class PremioDao {
+public class PremioDao extends Conexion{
+    public void registrar(PremioModelo pre) throws Exception{
+        try {
+            String sql = "INSERT INTO PREMIO(NOMPRE, DESPRE, IDTK) VALUES (?,?,?)";
+            
+            PreparedStatement st = this.getCnx().prepareStatement(sql);
+            st.setString(1, pre.getName());
+            st.setString(2, pre.getDesc());
+            st.executeUpdate();
+            st.close();
+        } catch (Exception e) {
+            System.out.println("Error en PremioDao-Registrar: "+ e.getMessage());
+        }finally{
+            this.cerrarCnx();
+        }
+    }
     
+    public void modificar(PremioModelo pre) throws Exception{
+        try {
+            String sql = "UPDATE PREMIO SET NOMPRE=?, DESPRE=? WHERE IDPRE=?";
+            PreparedStatement st = this.getCnx().prepareStatement(sql);
+            st.setString(1, pre.getName());
+            st.setString(2, pre.getDesc());
+            st.executeUpdate();
+            st.close();
+        } catch (Exception e) {
+            System.out.println("Error en PremioDao-Modificar: "+ e.getMessage());
+        }finally{
+            this.cerrarCnx();
+        }
+    }
+    
+    public void eliminar(PremioModelo pre) throws Exception{
+        try {
+            String sql = "DELETE FROM PREMIO WHERE IDEPRE=?";
+            PreparedStatement st = this.getCnx().prepareStatement(sql);
+            st.setInt(1, pre.getCod());
+            st.executeUpdate();
+            st.close();
+        } catch (Exception e) {
+            System.out.println("Error en PremioDao-Eliminar: "+ e.getMessage());
+        }finally{
+            this.cerrarCnx();
+        }
+    }
+    
+    public List<PremioModelo> listar() throws Exception{
+        List<PremioModelo> lista = null;
+        PremioModelo premio;
+        
+        try {
+            System.out.println("Se está realizando la consulta en Premio Dao");
+            String sql = "SELECT * FROM PREMIO";
+            Statement st = this.getCnx().createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            lista = new ArrayList();
+            while(rs.next()){
+                premio = new PremioModelo();
+                premio.setCod(rs.getInt("IDPRE"));
+                premio.setName(rs.getString("NOMPRE"));
+                premio.setDesc(rs.getString("DESPRE"));
+                lista.add(premio);
+                System.out.println("Enviando premio");
+            }
+            
+            rs.close();
+            st.close();
+        } catch (Exception e) {
+            System.out.println("Error en Premiodao-Listar: "+ e.getMessage());
+        }finally{
+            this.cerrarCnx();
+        }
+        return lista;
+    }
 }
